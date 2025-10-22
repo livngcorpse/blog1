4b4a6fd8857a5b231973197084ef38da4c659e36
Jett <arifmohammad2612@gmail.com>
2025-10-22 12:30:41 +0530

    Third commit

--- frontend/src/pages/SinglePost.js
+++ frontend/src/pages/SinglePost.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate, Link } from 'react-router-dom';
 import { postAPI, replyAPI, reportAPI, bookmarkAPI } from '../services/api';
 import UserAvatar from '../components/UserAvatar';
